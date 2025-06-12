e760f722f22744f73dbad5dab24ffd34e689be5d
leeejjju <leeejjju@gmail.com>
2025-06-12 10:06:26 +0900

    init

---
+++ todo-backend/src/test/java/com/leeejjju/todo/todobackend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeejjju.todo.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
